3ab34ca73ae9fa8f1f8c51f10b5ba11c5e4f3c89
LexSwed <oleksandr.shvechykov@pipedrive.com>
2020-02-03 21:53:45 +0200

    add apollo quick guide setup

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton'
+  }
+];
+
+const resolvers = {
+  Query: {
+    books: () => books
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
